cbf69e3d50b63f5605e8979de9167173513f4c46
leoubaque <leoubaque@hotmail.com>
2015-07-21 19:44:30 -0500

    Marco de la Aplicacion

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = require('express-partials')
 var routes = require('./routes/index');
 
 var app = express();
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(partials());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
